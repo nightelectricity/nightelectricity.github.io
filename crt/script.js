217844aeb40336ab874dfb1c98cd96d752120524
nightelectricity <st@nightelectricity.com>
2024-12-19 22:46:43 +0000

    Update script.js
    permalinks added

--- crt/script.js
+++ crt/script.js
@@ -61,8 +61,8 @@ if ( url.includes("posts/") ) {
 
 //Generate the Header HTML, a series of list items containing links.
 let headerHTML = '<ul> <li><a href="' + relativePath + '/">Home</a></li>' + 
-'<li><a href="' + relativePath + '/archive.html">Archive</a></li>' +
-'<li><a href="' + relativePath + '/about.html">About</a></li> </ul>';
+'<li><a href="' + relativePath + '/archive">Archive</a></li>' +
+'<li><a href="' + relativePath + '/about">About</a></li> </ul>';
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
 //Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
